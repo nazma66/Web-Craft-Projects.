0110c7067466fca05d84fc471fb8f51f285aa29d
Nazma Humayra <68192676+nazma66@users.noreply.github.com>
2023-11-28 22:15:04 +0600

    Add files via upload

---
+++ 50 Projects In 50 Days - HTML, CSS & JavaScript course/wavy transition/script.js
@@ -0,0 +1,12 @@
+const labels = document.querySelectorAll('.form-control label')
+
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span 
+        style="transition-delay:${idx * 10}ms">${letter}</span>`)
+        .join('')
+
+})
+//  idx * (here the number can be changed)
